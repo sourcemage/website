85f9242cc381046c27d7e362075fd7c24fe547a4
filesystem <unknown>
2021-06-22 04:04:41 +0000

    Add support for some Creole Additions

--- init.py
+++ init.py
@@ -7,6 +7,41 @@ from flup.server.fcgi import WSGIServer
 from wsgibrotli.br import BrotliMiddleware
 from wsgigzip.gzip import GzipMiddleware
 
+
+class SMGLWikiParser(hatta.WikiParser):
+    """ WikiParser with support for Creole Additions from 
+        http://www.wikicreole.org/wiki/CreoleAdditions """
+
+    markup_rules = hatta.WikiParser.markup_rules
+    punct = hatta.WikiParser.punct
+    # replaced by subscript
+    punct.pop(r",,")
+
+    @markup_rules(r"\^\^", 41)
+    def _line_superscript(self):
+        if 'sup' in self.stack:
+            return self.pop_to('sup')
+        else:
+            self.stack.append('sup')
+            return "<sup>"
+
+    @markup_rules(r",,", 42)
+    def _line_subscript(self):
+        if 'sub' in self.stack:
+            return self.pop_to('sub')
+        else:
+            self.stack.append('sub')
+            return "<sub>"
+
+
+class SMGLWikiPageWiki(hatta.WikiPageWiki):
+    
+    def __init__(self, *args, **kw):
+        super(SMGLWikiPageWiki, self).__init__(*args, **kw)
+
+        self.parser = SMGLWikiParser
+
+
 config = hatta.WikiConfig(
     site_name = 'Source Mage GNU/Linux',
     front_page = 'News',
@@ -22,5 +57,6 @@ config.parse_args()
 config.parse_files()
 config.sanitize()
 wiki = hatta.Wiki(config)
+wiki.mime_map['text/x-wiki'] = SMGLWikiPageWiki
 
 WSGIServer(GzipMiddleware(BrotliMiddleware(wiki.application, etag_alter=True))).run()
